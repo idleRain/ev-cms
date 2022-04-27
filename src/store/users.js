22ef29ce9f0fefc220cef9df27027499f8b9ecf9
idleRain <gold.experience@foxmail.com>
2022-04-27 21:58:39 +0800

    完成侧边栏模块

--- src/store/users.js
+++ src/store/users.js
@@ -1,14 +1,39 @@
+// axios 挂载到 Vue 实例上 , 但 vuex 跟 Vue 实例没有关系 , 所以需要重新引入
+import axios from "axios";
+
 export default {
     // 开启命名空间
-    namespaced:true,
+    namespaced: true,
     state: () => ({
-        token:''
+        token: '',
+        userinfo:{}
     }),
-    mutations:{
-        updateToken(state,token){
+    mutations: {
+        // 将 token 存放到 Vuex
+        updateToken(state, token) {
             state.token = token
+        },
+        // 将 用户数据存放到 Vuex
+        updateUserinfo(state, userinfo) {
+            state.userinfo = userinfo
         }
     },
-    actions:{},
-    getters:{}
+    // 异步或需处理的任务放在 actions 里面
+    actions: {
+        // content : 上下文对象 ( 小store )
+        getUserinfo(content) {
+            axios.get('/my/userinfo', {
+                // 携带请求头
+                headers: {
+                    Authorization: content.state.token
+                }
+            }).then(({data: res}) => {
+                // 如果没有获取到则终止运行
+                if (res.code !== 0) return
+                // 数据提交给 mutations 下的 updateUserinfo
+                content.commit('updateUserinfo',res.data)
+            })
+        }
+    },
+    getters: {}
 }
--- src/views/Main/Main.vue
+++ src/views/Main/Main.vue
@@ -3,7 +3,7 @@
     <!-- 头部区域 -->
     <el-header>
       <!-- 左侧的 logo -->
-      <img src="../../assets/images/logo.png" alt="" />
+      <img src="../../assets/images/logo.png" alt=""/>
       <!-- 右侧的菜单 -->
       <el-menu
           class="el-menu-top"
@@ -15,7 +15,7 @@
         <el-submenu index="1">
           <template slot="title">
             <!-- 头像 -->
-            <img src="../../assets/logo.png" alt="" class="avatar" />
+            <img src="@/assets/logo.png" alt="" class="avatar"/>
             <span>个人中心</span>
           </template>
           <el-menu-item index="1-1"><i class="el-icon-s-operation"></i>基本资料</el-menu-item>
@@ -27,7 +27,45 @@
     </el-header>
     <el-container>
       <!-- 侧边栏区域 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <!--        侧边栏用户名头像展示-->
+        <div class="user-box">
+          <!-- webpack 打包注意事项 ：如果路径里面是表达式或变量 ，webpack 不会进行打包 -->
+          <!-- <img :src="userinfo.user_pic || '@/assets/logo.png'" alt=""> -->
+          <img v-if="userinfo.user_pic" :src="userinfo.user_pic">
+          <img v-else src="@/assets/logo.png">
+          <span>欢迎 {{ userinfo.nickname || userinfo.username }}</span>
+        </div>
+        <!-- 左侧菜单栏 -->
+        <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            background-color="#23262e"
+            text-color="#fff"
+            active-text-color="#409eff">
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>文章管理</span>
+            </template>
+            <el-menu-item index="2-1">文章分类</el-menu-item>
+            <el-menu-item index="2-2">文章列表</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>个人中心</span>
+            </template>
+            <el-menu-item index="3-1">基本资料</el-menu-item>
+            <el-menu-item index="3-2">更换头像</el-menu-item>
+            <el-menu-item index="3-3">重置密码</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-container>
         <!-- 页面主体区域 -->
         <el-main>
@@ -41,9 +79,17 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
   name: 'Main',
-  methods:{
+  data(){
+    return {
+      // 准备数组装菜单栏数据
+      menu: []
+    }
+  },
+  methods: {
     logout() {
       this.$confirm('嘤嘤嘤，确定要退出登陆吗？ ꒰*´◒`*꒱ ', '退出登录', {
         confirmButtonText: '确定',
@@ -65,6 +111,24 @@ export default {
         })
       })
     }
+  },
+  // 页面加载时获取用户信息 , 需携带请求头并且存放到 Vuex 中 , 所以在 vuex 中发送请求
+  created() {
+    // 发送请求 ，获取菜单列表
+    this.$http.get('/my/menus', {
+      // 需要携带请求头
+      headers: {
+        Authorization: this.token
+      }
+    }).then(({data: res}) => {
+      if (res.code !== 0) return
+      this.menu = res.data
+    })
+
+    this.$store.dispatch('users/getUserinfo')
+  },
+  computed: {
+    ...mapState('users', ['userinfo', 'token'])
   }
 }
 </script>
@@ -72,20 +136,48 @@ export default {
 <style lang="less" scoped>
 .main-container {
   height: 100%;
+
   .el-header,
   .el-aside {
     background-color: #23262e;
   }
+
   .el-header {
     padding: 0;
     display: flex;
     justify-content: space-between;
   }
+
+  .el-aside {
+    .user-box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 70px;
+      border-top: 1px solid #000;
+      border-bottom: 1px solid #000;
+
+      img {
+        margin-right: 15px;
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        background: #fff;
+      }
+
+      span {
+        color: #fff;
+        font-size: 12px;
+      }
+    }
+  }
+
   .el-main {
     overflow-y: scroll;
     height: 0;
     background-color: #F2F2F2;
   }
+
   .el-footer {
     background-color: #eee;
     font-size: 12px;
@@ -102,5 +194,14 @@ export default {
   background-color: #fff;
   margin-right: 10px;
   object-fit: cover;
+}
+
+.el-menu-vertical-demo {
+  width: 200px;
+
+  span,
+  .el-menu-item {
+    font-size: 12px;
+  }
 }
 </style>
